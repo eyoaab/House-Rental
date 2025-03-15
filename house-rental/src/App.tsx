16eef9aedacd2c05c7728a6e82f62312e0908cad
eyoaab <eyobtariku48@gmail.com>
2025-03-15 13:59:40 +0300

    completed  why to choose page

--- house-rental/src/App.tsx
+++ house-rental/src/App.tsx
@@ -2,11 +2,13 @@
 import React from "react";
 import ServicesList from "./pages/services/services-page";
 import AppartmentList from "./pages/Apartments/apartments-page";
+import WhyChooseUs from "./pages/WhyToChooseUs/why-to-choose-us-page";
 
 const App: React.FC = () => {
   return (
     <div className="flex items-center justify-center flex-col w-screen lg:px-20 md:px-15 sm:px-10 px-5 bg-white">
-      <AppartmentList />
+      {/* <AppartmentList /> */}
+      <WhyChooseUs />
       <ServicesList />
     </div>
   );
